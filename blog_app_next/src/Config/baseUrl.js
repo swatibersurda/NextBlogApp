d8949d66b025c143167809ba33e745f1cf4601b7
swatibersurda <swatibersurda@gmail.com>
2023-04-06 01:25:38 +0530

    add

--- blog_app_next/src/Config/baseUrl.js
+++ blog_app_next/src/Config/baseUrl.js
@@ -1,2 +1,3 @@
-const baseUrl=process.env.NODE_ENV==="production" ?"https://advancedblogappswati.vercel.app/":"http://localhost:3000";
+// const baseUrl=process.env.NODE_ENV==="production" ? "https://advancedblogappswati.vercel.app":"http://localhost:3000";
+const baseUrl= "https://advancedblogappswati.vercel.app"
 export default baseUrl
